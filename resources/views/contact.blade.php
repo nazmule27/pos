7821fede331303d5805f51dbcc05dfe3c5da8217
Nazmul Haque <nlnazmul@gmail.com>
2016-02-07 16:45:18 +0600

    auto focus addded

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -19,7 +19,7 @@
                 {!! csrf_field() !!}
                 <div class="row">
                     <div class="form-group col-md-6">
-                        <input type="text" class="form-control custom-text" name="name" id="name" placeholder="Your Name *" required>
+                        <input type="text" class="form-control custom-text" name="name" id="name" placeholder="Your Name *" autofocus required>
                     </div>
                     <div class="form-group col-md-6">
                         <input type="email" class="form-control custom-text" name="email" id="email" placeholder="Your Email *" required>
